8c67839b5b13c408640dcdc824183c72b98487f5
aralujoseph <aralujoseph@gmail.com>
2022-09-08 17:03:31 -0700

    my commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,11 +5,12 @@
  *
  * * Return: 0 if exited properly, non-zero otherwise
  *
- *
  */
-int main(void)
-{
-        printf("with the proper grammer, but the outcome is a piece of art,\n")
 
-        return (0);
+int main(void)
+
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
 }
