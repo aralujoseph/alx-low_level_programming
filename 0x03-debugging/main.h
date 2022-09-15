3ec1443e2778c8364c39b12e7b9dbbe94a648c05
aralujoseph <aralujoseph@gmail.com>
2022-09-15 14:27:25 -0700

    my commit

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,6 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdio.h>
+ginclude <stdio.h>
 void positive_or_negative(int i);
 int largest_number(int, int, int);
 void print_remaining_days(int month, int day, int year);
