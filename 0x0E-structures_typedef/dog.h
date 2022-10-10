e0f46e40c23588bb5a838e849099a335aa45410c
aralujoseph <aralujoseph@gmail.com>
2022-10-10 16:45:13 -0700

    structures typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,17 +1,23 @@
-#include <stdlib>
-#include "dog.h"
+#ifndef _DOG_H_
+#define _DOG_H_
+
 /**
- * free_dog-free memory *@d
- * Description: free memory for struct
+ * dog_t - Typedef for struct dog
  */
+typedef struct dog dog_t;
 
-
-void free_dog(dog_t *d);
-        if (d)
-        (
-                free(d->name);
-                free(d->owner);
-                free(d);
-        )
-
-
+/**
+ * struct dog - struct that stores some information of a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: struct called "dog" that stores its name, its age
+ * and the name of its owner.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
